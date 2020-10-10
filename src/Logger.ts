bee249dafd2323122b241dabc26660f857859dc6
Lukas <lukas_sturm@yahoo.de>
2020-10-11 00:13:48 +0200

    better formatting

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,12 +1,20 @@
 
 import winston = require('winston');
- 
+
+const alignedWithColorsAndTime = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.align(),
+  winston.format.printf((info) => {
+    const { timestamp, level, message, ...args } = info;
+    const ts = timestamp.slice(0, 19).replace('T', ' ');
+    return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+  }),
+);
+
 export const Log: winston.Logger = winston.createLogger({
   level: 'silly',
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
+  format: alignedWithColorsAndTime,
   transports: [
     // new winston.transports.File({ filename: 'error.log', level: 'error' }),
     // new winston.transports.File({ filename: 'combined.log' }),
@@ -16,10 +24,7 @@ export const Log: winston.Logger = winston.createLogger({
 
 export const LogAPI: winston.Logger = winston.createLogger({
   level: 'silly',
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
+  format: alignedWithColorsAndTime,
   transports: [
     // new winston.transports.File({ filename: 'error.log', level: 'error' }),
     // new winston.transports.File({ filename: 'combined.log' }),
